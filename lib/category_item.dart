a78d08bf1b1ba2666d73ea4799c27e40d0a68a64
Abhishek Ranjan <ab85hishek@gmail.com>
2020-09-10 13:51:05 +0530

    updating multiple screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,19 +1,19 @@
-import './category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen();
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './category_meals_screen.dart';
 import 'package:flutter/material.dart';
 import './categories_screen.dart';
 
@@ -31,6 +32,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
